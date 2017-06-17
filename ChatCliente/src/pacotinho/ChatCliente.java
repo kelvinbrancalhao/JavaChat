57eb48d7dc1f7722bd6a13d94d29d076eec7b6a8
Kelvin Brancalhao <kelvinbrancalhao@gmail.com>
2017-06-17 17:57:23 -0300

    atualizando codigo

---
+++ ChatCliente/src/pacotinho/ChatCliente.java
@@ -0,0 +1,10 @@
+package pacotinho;
+
+public class ChatCliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ChatServidor/src/pacotera/ChatServidor.java
@@ -0,0 +1,10 @@
+package pacotera;
+
+public class ChatServidor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
